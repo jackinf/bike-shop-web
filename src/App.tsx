7ff80e679cd969d0045cf801b2bff5a6a5c91bca
Jevgeni Rumjantsev <zeka.rum@gmail.com>
2019-09-12 19:56:13 +0200

    Log in and Log out

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,38 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import firebase from 'firebase/app';
-import GoogleLogin from 'react-google-login';
+import {GoogleLogin, GoogleLogout} from 'react-google-login';
+
 import { config } from './index';
 
 const App: React.FC = () => {
   const [token, setToken] = useState("");
 
+  useEffect(() => {
+    const unsubscribe = firebase.auth().onAuthStateChanged(async (user) => {
+      if (user) {
+        setToken(await user.getIdToken());
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    }
+  }, []);
+
   const handleGoogleLoginSuccess = async (payload: any) => {
-    console.log(`Success!`, payload);
     const credential = firebase.auth.GoogleAuthProvider.credential(payload.getAuthResponse().id_token);
     const result = await firebase.auth().signInWithCredential(credential);
-    console.log(result);
-    const user: any = result.user;
-    const token = await user.getIdToken();
-    setToken(token);
+    const user = result.user;
+    if (user) {
+      setToken(await user.getIdToken());
+    }
   };
 
-  const handleGoogleLoginFailure = (error: any) => {
-    console.error('Failed', error);
+  const handleGoogleLoginFailure = (error: any) => console.error('Failed', error);
+
+  const handleGoogleSignOut = async () => {
+    await firebase.auth().signOut();
+    setToken("");
   };
 
   const googleClientId = config.google.clientId;
@@ -27,18 +42,28 @@ const App: React.FC = () => {
 
   return (
     <div className="App">
-      <GoogleLogin
-        clientId={googleClientId}
-        buttonText="Login"
-        onSuccess={handleGoogleLoginSuccess}
-        onFailure={handleGoogleLoginFailure}
-        cookiePolicy={'single_host_origin'}
-      />
-      <p>
-        {token}
-      </p>
+      {!!token ? (
+        <div>
+          <GoogleLogout
+            clientId={googleClientId}
+            buttonText="Logout"
+            onLogoutSuccess={handleGoogleSignOut}
+          />
+          <p>
+            {token}
+          </p>
+        </div>
+      ) : (
+        <GoogleLogin
+          clientId={googleClientId}
+          buttonText="Login"
+          onSuccess={handleGoogleLoginSuccess}
+          onFailure={handleGoogleLoginFailure}
+          cookiePolicy={'single_host_origin'}
+        />
+      )}
     </div>
   );
-}
+};
 
 export default App;
